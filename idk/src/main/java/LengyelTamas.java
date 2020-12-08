b8985c1112fbe0ae0a41b5c627173666a23cbc2e
Tauter <harangi.ftam@gmail.com>
2020-12-08 10:48:56 +0100

    Második függvény hozzáadása 	Egyszerű hozzáadása

---
+++ idk/src/main/java/LengyelTamas.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author haran
+ */
+public class LengyelTamas {
+    public static int osszead(int x, int y){
+        return x+y;
+    }
+    public static void main(String[] args) {
+        System.out.println("25 + 25 = " + osszead(25, 25));
+    }
+}
